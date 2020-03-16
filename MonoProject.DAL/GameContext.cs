b9246600f733c16a27d8d32f2479e7f2af891a4d
Pavle Dević <pdevic@tutanota.com>
2020-03-16 16:18:12 +0100

    Added DAL GameContext class

---
+++ MonoProject.DAL/GameContext.cs
@@ -0,0 +1,23 @@
+namespace MonoProject.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using MonoProject.Model.Common;
+
+    public class GameContext : DbContext
+    {
+        public GameContext()
+            : base("name=GameContext")
+        {
+        }
+
+        public DbSet<IGameInfo> GameInfos { get; set; }
+        public DbSet<IPlayerCountTag> PlayerCountTags { get; set; }
+
+        public DbSet<IGameInfoPlayerCountTag> GameInfoPlayerCounts { get; set; }
+
+    }
+
+}
